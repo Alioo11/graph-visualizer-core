7649a64b0170c939f034f109e122d83fe2198648
Alioo11 <110mors110@gmail.com>
2024-06-10 22:14:05 +0330

    feat: update view model

--- src/models/View/index.ts
+++ src/models/View/index.ts
@@ -1,20 +1,41 @@
+import { NoneToVoidFunction, Nullable } from "ts-wiz";
 import { IDataStructure, IView } from "../../types";
 
-class View<T> implements IView<T> {
-  documentRef: HTMLDivElement;
+abstract class View<T> implements IView<T> {
+  private documentRootRef: Nullable<HTMLDivElement> = null;
+  abstract documentRef: Nullable<HTMLDivElement>;
   visible: boolean = true;
-  dataStructure: IDataStructure<T>;
-  constructor(
-    documentRef: IView<T>["documentRef"],
-    dataStructure: IDataStructure<T>
-  ) {
-    this.documentRef = documentRef;
-    this.dataStructure = dataStructure;
-  }
+  abstract dataStructure: IDataStructure<T>;
+
+  abstract onReady: Nullable<NoneToVoidFunction>;
+
+  private createWrapperElement = (rootElement: HTMLDivElement) => {
+    const documentRefElement = document.createElement("div");
+    documentRefElement.setAttribute("id", "element");
+    documentRefElement.setAttribute("class" , "view-container")
+
+    this.documentRootRef = rootElement;
+    this.documentRootRef.classList.add("view-root")
+
+    this.documentRef = documentRefElement;
+    this.documentRootRef.appendChild(documentRefElement);
+  };
+
+  private initializeWrapperElements = () => {
+    const visibilityToggleButton = document.createElement("button");
+    visibilityToggleButton.textContent = "toggle";
+    this.documentRootRef?.appendChild(visibilityToggleButton);
+  };
+
+  init = (rootHTMLElement: HTMLDivElement) => {
+    this.createWrapperElement(rootHTMLElement);
+    this.initializeWrapperElements();
+    this.onReady?.();
+  };
+
   toggleVisible = () => {
     this.visible = !this.visible;
   };
 }
-
 
 export default View;
