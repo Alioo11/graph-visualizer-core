f83193c446ed36110bee0593f5c44158babd490f
Alioo11 <110mors110@gmail.com>
2024-06-13 10:48:46 +0330

    feat: add events to Coordinated Graph

--- src/models/Graph/Coordinated.ts
+++ src/models/Graph/Coordinated.ts
@@ -1,23 +1,47 @@
+import { Nullable } from "ts-wiz";
 import Graph from ".";
-
-type CoordinatedGraphVertexState = "blank" | "visited";
-
-interface ICoordinatedGraphVertex {
-  state: CoordinatedGraphVertexState;
-  x: number;
-  y: number;
-}
-
-interface ICoordinatedGraphEdge {
-  wight: number;
-}
 
 class CoordinatedGraph extends Graph<
   ICoordinatedGraphVertex,
   ICoordinatedGraphEdge
 > {
+  private _coordinatedVertexEventMap = new Map<
+    keyof CoordinatedVertexEvents,
+    Array<(data: any) => void>
+  >();
 
-  addEvent= ()=>{}
+  entryVertex: Nullable<CoordinatedGraphVertex> = null;
+  targetVertex: Array<CoordinatedGraphVertex> = [];
+
+  updateVertex(
+    vertex: CoordinatedGraphVertex,
+    cb: (data: CoordinatedGraphVertex["data"]) => CoordinatedGraphVertex["data"]
+  ) {
+    const newData = cb(vertex.data);
+
+    const didPositionChange =
+      newData.x !== vertex.data.x || newData.y !== vertex.data.y;
+    const didStateChange = newData.state !== vertex.data.state;
+
+    vertex.data = newData;
+
+    if (didPositionChange)
+      this._coordinatedVertexEventMap
+        .get("position-change")
+        ?.forEach((cb) => cb(vertex));
+    if (didStateChange)
+      this._coordinatedVertexEventMap
+        .get("state-change")
+        ?.forEach((cb) => cb(vertex));
+  }
+
+  onVertex = <T extends keyof CoordinatedVertexEvents>(
+    eventType: T,
+    callback: (data: CoordinatedVertexEvents[T]) => void
+  ) => {
+    const events = this._coordinatedVertexEventMap.get(eventType) || [];
+    this._coordinatedVertexEventMap.set(eventType, [...events, callback]);
+  };
 }
 
 export default CoordinatedGraph;
