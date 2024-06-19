3d5e4683d3650454d409c331fe69bc25474db138
Alioo11 <110mors110@gmail.com>
2024-06-19 11:45:55 +0330

    add new util for hashing

--- src/utils/Text.ts
+++ src/utils/Text.ts
@@ -1,15 +1,24 @@
 class TextUtil {
-  static verticalAdd(str1: string, str2: string) {
+  /**
+   * @description generates a hash from two give strings "a,b", no matter the order of call the result of calling bidirectionalHash(a,b) or bidirectionalHash(b,a) is the same.
+   */
+  static bidirectionalHash(strA: string, strB: string) {
     let result = "";
-    for (let i = 0; i < Math.max(str1.length, str2.length); i++) {
-      const charCodeStr1 = str1.charCodeAt(i) || 0;
-      const charCodeStr2 = str2.charCodeAt(i) || 0;
+    for (let i = 0; i < Math.max(strA.length, strB.length); i++) {
+      const charCodeStr1 = strA.charCodeAt(i) || 0;
+      const charCodeStr2 = strB.charCodeAt(i) || 0;
       const xorValue = ((charCodeStr1 + charCodeStr2) % 74) + 48;
       result += String.fromCharCode(xorValue);
     }
     return result;
   }
-}
 
+  /**
+   * @description generates a hash from two given strings
+   */
+  static directionalHash(strA: string, strB: string) {
+    return strA.concat("-" + strB);
+  }
+}
 
 export default TextUtil;
