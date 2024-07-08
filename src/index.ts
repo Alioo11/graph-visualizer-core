3535a263a68eab0c9d8c975214b8e8345194a81a
Alioo11 <110mors110@gmail.com>
2024-07-08 17:13:53 +0330

    fix: fix module export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "./scss/main.scss";
 import 'boxicons'
 import Stage from "./models/Stage";
-import Visualization from "./models/Visualization/Dijkstra";
+import Visualization from "./models/Visualization/PathFinding/index";
 
 export { Stage, Visualization };
 
