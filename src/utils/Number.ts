0dcee9168d6624f2585ce3032cb8ac532701a5c6
Alioo11 <110mors110@gmail.com>
2024-07-16 15:58:40 +0330

    feat: add number utility

--- src/utils/Number.ts
+++ src/utils/Number.ts
@@ -1,8 +1,12 @@
 class NumberUtils {
-    static randomNumberBetween(min:number,max:number){
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-}
+  static randomNumberBetween(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
+  static maybe(radio: number = 0.5) {
+    if (radio < 0 || radio > 1) throw new Error("radio must be a value between 0 and 1");
+    return Math.random() > radio;
+  }
+}
 
 export default NumberUtils;
