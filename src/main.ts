327bbb93b2053f4d5cb07ef5228505ff2c15ac28
Alioo11 <110mors110@gmail.com>
2024-08-10 14:55:34 +0330

    update: main(playground)

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import Stage from "@models/Stage";
 import type { NoneToVoidFunction } from "ts-wiz";
 import "./scss/main.scss";
 import "boxicons";
+import wait from "@utils/wait";
+import NumberUtils from "@utils/Number";
 
 const appElement = document.querySelector("#app")! as HTMLDivElement;
 
 let stage = Stage.init(appElement);
 
-let dds = new DijkstraVisualization();
+let vis = new DijkstraVisualization();
+vis.mainView.on("ready", () => {
+  vis.mainView.translateTo(0,0)
+});
 
-stage.visualization = dds;
+
+stage.visualization = vis;
 
 const body = $("body");
 
@@ -22,10 +28,34 @@ const addBtn = (name: string, cb: NoneToVoidFunction) => {
   $("#btn-container").append(btn);
 };
 
-addBtn("start", () => dds.start());
-addBtn("pause", () => dds.pause());
-addBtn("step", () => dds.step());
-addBtn("generate maze", () => dds.generateRecursiveBacktrackingMaze());
-addBtn("toggle ruler", () => (dds.mainView.showRuler = !dds.mainView.showRuler));
-addBtn("toggle grid", () => (dds.mainView.showGrid = !dds.mainView.showGrid));
-addBtn("create new Graph", () => dds.createGraph("randomized", { size: 100 }));
+vis.speed = "slow";
+
+const doSomething = async () => {
+  const someRandomCoords = Array.from(new Array(100).keys()).map((i) => [
+    NumberUtils.randomNumberBetween(-5000, 5000),
+    NumberUtils.randomNumberBetween(-5000, 5000),
+  ]);
+  for (let value of someRandomCoords) {
+    vis.mainView.translateTo(value[0], value[1]);
+    await wait(2000);
+    
+  }
+};
+
+addBtn("start", () => vis.start());
+addBtn("pause", () => vis.pause());
+addBtn("step", () => vis.step());
+addBtn("generate maze", () => {
+  vis.speed = "normal";
+  vis.generateRecursiveBacktrackingMaze();
+  // dds.speed = "slow";
+});
+addBtn("toggle ruler", () => (vis.mainView.showRuler = !vis.mainView.showRuler));
+addBtn("toggle grid", () => (vis.mainView.showGrid = !vis.mainView.showGrid));
+addBtn("toggle grid", doSomething);
+addBtn("rand", () => vis.createGraph("randomized" , {size:2000}));
+
+vis.graph.onDijkstra("edge-change" , (e)=>{
+  const [x, y] = [e.from.data.x, e.from.data.y];
+  vis.mainView.translateTo(x,y, 300)
+})
